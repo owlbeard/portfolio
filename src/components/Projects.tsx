b343b22d04cd57d7cc533d0585834ef80b8fce88
Owlbeard <omeraltun96@gmail.com>
2023-06-01 17:54:04 +0300

    Add links to projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,14 +13,16 @@ export default function Projects() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1, ease: 'easeInOut' }}
-      className="flex flex-col items-center justify-center"
+      className="my-12 flex flex-col items-center justify-center"
     >
       <div className="grid grid-cols-responsive gap-4">
-        <div>
-          <h2 className="2xl:text-6xl xl:text-4xl l:text-4xl md:text-4xl sm:text-4xl text-xl">
+        <div className="flex flex-col gap-4">
+          <h2 className="2xl:text-6xl xl:text-4xl l:text-4xl md:text-4xl sm:text-4xl text-3xl">
             Todo List
           </h2>
-          <img src={projects[0]} alt="Todo List" />
+          <a href="https://owlbeard.github.io/Todo-List/">
+            <img src={projects[0]} alt="Todo List" />
+          </a>
           <p>
             Simple todo list app where you can create edit and delete tasks,
             create edit or delete projects add tasks to the projects etc.
@@ -28,28 +30,32 @@ export default function Projects() {
             npm packages were used.
           </p>
         </div>
-        <div>
-          <h2 className="2xl:text-6xl xl:text-4xl l:text-4xl md:text-4xl sm:text-4xl text-xl">
+        <div className="flex flex-col gap-4">
+          <h2 className="2xl:text-6xl xl:text-4xl l:text-4xl md:text-4xl sm:text-4xl text-3xl">
+            Shopping Cart
+          </h2>
+          <a href="https://owlbeard.github.io/shopping-cart/">
+            <img src={projects[1]} alt="Shopping Cart" />
+          </a>
+          <p>
+            A shopping cart app where you can add any amount of items to your
+            cart, you can also remove items or add more items in the checkout
+            page. Created with HTML, CSS, JS, React.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h2 className="2xl:text-6xl xl:text-4xl l:text-4xl md:text-4xl sm:text-4xl text-3xl">
             Weather App
           </h2>
-          <img src={projects[2]} alt="Weather App" />
+          <a href="https://owlbeard.github.io/weather-app-react/">
+            <img src={projects[2]} alt="Weather App" />
+          </a>
           <p>
             Weather app that fetches data from WeatherAPI asynchronously to get
             weather data of any city the user searched in the search bar.
             Depending on the state of the weather of selected city, background
             will change accordingly! Created with vanilla JS, HTML and CSS.
             Bundled with WebPack.
-          </p>
-        </div>
-        <div>
-          <h2 className="2xl:text-6xl xl:text-4xl l:text-4xl md:text-4xl sm:text-4xl text-xl">
-            Shopping Cart
-          </h2>
-          <img src={projects[1]} alt="Shopping Cart" />
-          <p>
-            A shopping cart app where you can add any amount of items to your
-            cart, you can also remove items or add more items in the checkout
-            page. Created with HTML, CSS, JS, React.
           </p>
         </div>
       </div>
